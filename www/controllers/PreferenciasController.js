78b552423253d77cf8795371cd9893d47ac2cbf0
Danilo Savio <danilosavio25@gmail.com>
2015-10-15 12:12:45 -0300

    Configuração de notificações para Rodizio e cancelamento das notificações.

--- www/controllers/PreferenciasController.js
+++ www/controllers/PreferenciasController.js
@@ -2,40 +2,87 @@ app.controller('PreferenciasCtrl', ['$scope', 'PreferenciasService', '$statePara
 
 	$scope.preferencias = PreferenciasService.getPreferencias();
 
-	$scope.exibirNotificacao = function(){
-			 var alarmTime = new Date();
-		//	 alarmTime.setSeconds(alarmTime.getSeconds() + 5);
-			 $cordovaLocalNotification.add({
-					 id: 1,
-					 date: alarmTime,
-					 message: "This is a message",
-					 title: "This is a title",
-					 autoCancel: true,
-					 sound: null,
-					  icon: "../img/logo.png"
-			 }).then(function () {
-					 console.log("The notification has been set");
+
+		$scope.gerenciarNotificacoes = function(preferencia, isChecked){
+				console.log(preferencia, isChecked);
+
+				if(isChecked){
+					alertarRodizio(preferencia.apolice + 'R', preferencia.rodizioId);
+				}else{
+					cancelarNotificacoes(preferencia.apolice);
+				}
+
+		}
+
+
+
+
+		var alertarRodizio = function(apolice,rodizioId){
+				var hoje = new Date();
+				var diaSemana = hoje.getDay();
+
+				if(rodizioId == diaSemana){
+					console.log('dia semana ok');
+					agendarNotificacao(apolice + 'R', hoje, 'Alerta Rodízio','Fique atento ao horário de circulação do rodízio.', 'week');
+				}else{
+						var dataAgendamento;
+						var data = new Date();
+						var diaCorreto = false;
+						while(!diaCorreto){
+							data.setDate(data.getDate() + 1);
+						    if(data.getDay() == rodizioId){
+						    	dataAgendamento = data;
+						        diaCorreto = true;
+						    }
+							}
+							console.log("Alarme agendado para" + dataAgendamento);
+							agendarNotificacao(apolice + 'R', dataAgendamento, 'Alerta Rodízio','Fique atento ao horário de circulação do rodízio.', 'week');
+				}
+		}
+
+
+		var cancelarNotificacoes = function(apolice){
+
+			var notificationIds = [apolice + 'R', apolice + 'V']
+
+			 $cordovaLocalNotification.cancel(notificationIds)
+			 .then(function (result) {
+					console.log(result);
 			 });
-		};
+		}
 
 
-
-
-		$scope.agendarNotificacao = function (data) {
-				var now = new Date().getTime();
-				 var _10SecondsFromNow = new Date(now + 10 * 1000);
-
+		var agendarNotificacao = function (id, data, titulo, mensagem, repetir) {
+				// var now = new Date().getTime();
+				//  var _10SecondsFromNow = new Date(now + 10 * 1000);
+				console.log(id + data + titulo + mensagem);
 				 $cordovaLocalNotification.schedule({
-					 id: 2,
-					 title: 'Vencimento Apólice',
-					 text: 'Sua apólice vencerá no dia x',
-					 at: _10SecondsFromNow,
-					  icon: "../img/logo.png"
+					 id: id,
+					 title: titulo,
+					 text: mensagem,
+					 at: data,
+					  icon: '../img/logo.png',
+						every: repetir
 				 }).then(function (result) {
 					 		console.log(result);
 				 });
 	   };
 
+		 $scope.exibirNotificacao = function(){
+	 			 var alarmTime = new Date();
+	 		//	 alarmTime.setSeconds(alarmTime.getSeconds() + 5);
+	 			 $cordovaLocalNotification.add({
+	 					 id: 1,
+	 					 date: alarmTime,
+	 					 message: "This is a message",
+	 					 title: "This is a title",
+	 					 autoCancel: true,
+	 					 sound: null,
+	 					  icon: "../img/logo.png"
+	 			 }).then(function () {
+	 					 console.log("The notification has been set");
+	 			 });
+	 		};
 
 
 }]);
--- www/services/PreferenciasService.js
+++ www/services/PreferenciasService.js
@@ -1,24 +1,29 @@
 app.factory('PreferenciasService', function () {
+
+	// rodizioId Domingo = 0, Segunda-feira = 1, Terça = 2, Quarta = 3, Quinta = 4, Sexta = 5, Sabado = 6;
+
 	var preferencias = [{
 	id: 1,
 	apolice: '34313124',
 	IPVA: 'OK',
 	validade: '50 dias para vencimento da apólice.',
-	rodizio: 'Terça-feira'
+	rodizio: 'Terça-feira',
+	rodizioId: 2
 },
 {
 	id: 2,
 	apolice: '34312125',
 	IPVA: 'OK',
 	validade: '180 dias para vencimento da apólice.',
-	rodizio: 'Quinta-feira'
+	rodizio: 'Quinta-feira',
+	rodizioId: 4
 }]
 
 	var getPreferencias = function () {
 		return preferencias;
 	};
 
-	
+
 
 	return {
 		getPreferencias: getPreferencias
