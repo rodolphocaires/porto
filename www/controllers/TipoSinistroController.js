b56cd7a0238b690059d040346c20161a951bbd28
julio.coelho@yoopye.com.br <OpenShiftSenha>
2015-10-21 01:27:31 -0200

    Melhorias no formulário de colisão

--- www/controllers/TipoSinistroController.js
+++ www/controllers/TipoSinistroController.js
@@ -1,25 +1,35 @@
-app.controller('TipoSinistroCtrl', ['$scope', 'SinistroService', 'ApoliceService','$ionicModal', '$stateParams', function ($scope, SinistroService, ApoliceService, $ionicModal, $stateParams) {
+app.controller('TipoSinistroCtrl', ['$scope', 'SinistroService', 'ApoliceService', '$ionicModal', '$stateParams', '$ionicSlideBoxDelegate', function ($scope, SinistroService, ApoliceService, $ionicModal, $stateParams, $ionicSlideBoxDelegate) {
+	$scope.step = 0;
 
-	
-	 $scope.showModalSinistro = function(pagina){
-		$ionicModal.fromTemplateUrl('templates/form-'+ pagina +'.html', {
+	$scope.showModalSinistro = function (pagina) {
+		$ionicModal.fromTemplateUrl('templates/form-' + pagina + '.html', {
 			scope: $scope,
 			animation: 'slide-in-up'
 		}).then(function (modal) {
-			 $scope.novoSinistroModal = modal;
-			 showModal();
+			$scope.novoSinistroModal = modal;
+			showModal();
 		});
-	}
-	 
-	 var showModal = function(){
-		 $scope.novoSinistroModal.show();
-	 }
+	};
 
-	
-	 
-		var apoliceId = $stateParams.apoliceId;
-	if(apoliceId){
-		$scope.apolice =  ApoliceService.getApolice(apoliceId);	
+	var showModal = function () {
+		$scope.novoSinistroModal.show();
+	};
+
+	$scope.next = function () {
+		$ionicSlideBoxDelegate.next();
+	};
+
+	$scope.previous = function () {
+		$ionicSlideBoxDelegate.previous();
+	};
+
+	$scope.stepChanged = function (index) {
+		$scope.step = index;
+	};
+
+	var apoliceId = $stateParams.apoliceId;
+	if (apoliceId) {
+		$scope.apolice = ApoliceService.getApolice(apoliceId);
 	}
-	
+
 }]);
