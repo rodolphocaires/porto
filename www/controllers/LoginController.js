d86a6022948f502b6993fa4298a3f6cacea2b72d
julio.coelho@yoopye.com.br <OpenShiftSenha>
2015-10-22 01:53:51 -0200

    Form de login

--- www/controllers/LoginController.js
+++ www/controllers/LoginController.js
@@ -1,3 +1,22 @@
-app.controller('LoginCtrl', ['$scope', '$sce', function ($scope, $sce) {
-	
+app.controller('LoginCtrl', ['$scope', '$ionicPopup', '$timeout', function ($scope, $ionicPopup, $timeout) {
+	var loginPopup = null;
+
+	$scope.loginPorto = function () {
+		loginPopup = $ionicPopup.show({
+			templateUrl: 'templates/login-popup.html',
+			cssClass: 'login-popup',
+			scope: $scope
+		});
+	};
+
+	$scope.cancelLogin = function () {
+		loginPopup.close();
+	};
+
+	$scope.authenticate = function () {
+		loginPopup.close();
+		$timeout(function () {
+			$scope.closeLogin();
+		}, 500);
+	};
 }]);
